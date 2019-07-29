8395f0d87691ef474041b5419efe553abc407eba
Mikhail Sementsov <fewebdev@gmail.com>
2019-07-29 21:27:53 +0300

    debug

--- src/server/controllers/esp.js
+++ src/server/controllers/esp.js
@@ -3,19 +3,21 @@ const { createRequest, l } = require("../../utils");
 const baseURL = "http://192.168.0.13:8080";
 const request = createRequest({ baseURL });
 
+let st = false;
+
 const getHandler = async (req, res) => {
   const { state } = req.query;
 
-  let status = false;
-  try {
-    status = await request.get("/esp", {
-      state
-    });
-  } catch (e) {
-    l(e);
-  }
-
-  res.send(status);
+  // let status = false;
+  // try {
+  //   status = await request.get("/esp", {
+  //     state
+  //   });
+  // } catch (e) {
+  //   l(e);
+  // }
+  st = !st;
+  res.send(st);
 };
 
 module.exports = { getHandler };
